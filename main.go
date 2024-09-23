e0fc282e05a699674d1c2e459ce0d8773faf4e21
Yiren <l6mTbdko9Nza@gmail.com>
2024-09-23 13:30:53 +0700

    fix update

--- main.go
+++ main.go
@@ -1,98 +1,97 @@
 package main
 
 import (
-  "database/sql"
-  "fmt"
-  "log"
+	"database/sql"
+	"fmt"
+	"log"
 
-  _ "github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
 const (
-  host     = "localhost"  // or the Docker service name if running in another container
-  port     = 5432         // default PostgreSQL port
-  user     = "myuser"     // as defined in docker-compose.yml
-  password = "mypassword" // as defined in docker-compose.yml
-  dbname   = "mydatabase" // as defined in docker-compose.yml
+	host     = "localhost"  // or the Docker service name if running in another container
+	port     = 5432         // default PostgreSQL port
+	user     = "myuser"     // as defined in docker-compose.yml
+	password = "mypassword" // as defined in docker-compose.yml
+	dbname   = "mydatabase" // as defined in docker-compose.yml
 )
 
 type Product struct {
-  ID       int
-  Name     string
-  Price    int
+	ID    int
+	Name  string
+	Price int
 }
 
 var db *sql.DB
 
 func main() {
-  // Connection string
-  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-    "password=%s dbname=%s sslmode=disable",
-    host, port, user, password, dbname)
+	// Connection string
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
 
-  // Open a connection
-  sdb, err := sql.Open("postgres", psqlInfo)
-  if err != nil {
-    log.Fatal(err)
-  }
+	// Open a connection
+	sdb, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  db = sdb;
-  // Check the connection
-  err = db.Ping()
-  if err != nil {
-    log.Fatal(err)
-  }
+	db = sdb
+	// Check the connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  fmt.Println("Successfully connected!")
+	fmt.Println("Successfully connected!")
 
-  // err = createProduct(&Product{Name: "nackja",Price: 12345});
+	// err = createProduct(&Product{Name: "nackja",Price: 12345});
 
-  product,err :=getProduct(2)
-  fmt.Println("GET SUCCESS ",product)
+	product, err := getProduct(2)
+	fmt.Println("GET SUCCESS ", product)
 
-  err = updateProduct(2,&Product{Name: "nacklove",Price :1233})
+	err = updateProduct(2, &Product{Name: "nacklove", Price: 1233})
 
-  if err != nil {
-    log.Fatal(err)
-  }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  fmt.Println("Create Success")
+	fmt.Println("Create Success")
 }
 
-func createProduct (product *Product) error {
+func createProduct(product *Product) error {
 
-  _,err := db.Exec(
-    "INSERT INTO public.products(name, price)VALUES ($1, $2);",
-  product.Name,
-  product.Price,
-  )
+	_, err := db.Exec(
+		"INSERT INTO public.products(name, price)VALUES ($1, $2);",
+		product.Name,
+		product.Price,
+	)
 
-  return err
+	return err
 
 }
 
-func getProduct (id int) (Product,error) {
-  var p Product
-  row := db.QueryRow("SELECT id,name,price FROM products WHERE id=$1;",id)
+func getProduct(id int) (Product, error) {
+	var p Product
+	row := db.QueryRow("SELECT id,name,price FROM products WHERE id=$1;", id)
 
-  err := row.Scan(&p.ID,&p.Name,&p.Price)
-  
-  if err != nil{
-    return Product{},err
-  }
+	err := row.Scan(&p.ID, &p.Name, &p.Price)
 
+	if err != nil {
+		return Product{}, err
+	}
 
-  return p,nil
+	return p, nil
 }
 
-func updateProduct (id int ,product *Product) error {
+func updateProduct(id int, product *Product) error {
 
-  _,err := db.Exec(
-    "UPDATE public.products SET name=$1, price=$2 WHERE id=$3;",
-  product.Name,
-  id,
-  )
+	_, err := db.Exec(
+		"UPDATE public.products SET name=$1, price=$2 WHERE id=$3;",
+		product.Name,
+		id,
+	)
 
-  return err
+	return err
 
 }
