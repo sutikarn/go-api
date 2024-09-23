09f2b2f4d98f8a281cee1b13325bae88280bc3da
{sutikarn <l6mTbdko9Nza@gmail.com>
2024-09-23 15:59:42 +0700

    update crud api

--- main.go
+++ main.go
@@ -1,98 +1,128 @@
 package main
 
 import (
-  "database/sql"
-  "fmt"
-  "log"
+	"database/sql"
+	"fmt"
+	"log"
+	"strconv"
 
-  _ "github.com/lib/pq"
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
 )
 
 const (
-  host     = "localhost"  // or the Docker service name if running in another container
-  port     = 5432         // default PostgreSQL port
-  user     = "myuser"     // as defined in docker-compose.yml
-  password = "mypassword" // as defined in docker-compose.yml
-  dbname   = "mydatabase" // as defined in docker-compose.yml
+	host     = "localhost"  // or the Docker service name if running in another container
+	port     = 5432         // default PostgreSQL port
+	user     = "myuser"     // as defined in docker-compose.yml
+	password = "mypassword" // as defined in docker-compose.yml
+	dbname   = "mydatabase" // as defined in docker-compose.yml
 )
 
 type Product struct {
-  ID       int
-  Name     string
-  Price    int
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Price int    `json:"price"`
 }
 
 var db *sql.DB
 
 func main() {
-  // Connection string
-  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-    "password=%s dbname=%s sslmode=disable",
-    host, port, user, password, dbname)
+	// Connection string
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
 
-  // Open a connection
-  sdb, err := sql.Open("postgres", psqlInfo)
-  if err != nil {
-    log.Fatal(err)
-  }
+	// Open a connection
+	sdb, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  db = sdb;
-  // Check the connection
-  err = db.Ping()
-  if err != nil {
-    log.Fatal(err)
-  }
+	db = sdb
 
-  fmt.Println("Successfully connected!")
+	defer db.Close()
+	// Check the connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  // err = createProduct(&Product{Name: "nackja",Price: 12345});
+	fmt.Println("Successfully connected!")
 
-  product,err :=getProduct(2)
-  fmt.Println("GET SUCCESS ",product)
+	app := fiber.New()
 
-  err = updateProduct(2,&Product{Name: "nacklove",Price :1233})
+	app.Get("/product/:id", getProductHandler)
+  app.Get("/products/", getProductsHandler)
+	app.Post("/product/", createProductHandler)
+  app.Put("/product/:id", updateProductHandler)
+  app.Delete("product/:id",deleteProductHandler)
 
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  fmt.Println("Create Success")
-}
-
-func createProduct (product *Product) error {
-
-  _,err := db.Exec(
-    "INSERT INTO public.products(name, price)VALUES ($1, $2);",
-  product.Name,
-  product.Price,
-  )
-
-  return err
+	app.Listen(":8080")
 
 }
 
-func getProduct (id int) (Product,error) {
-  var p Product
-  row := db.QueryRow("SELECT id,name,price FROM products WHERE id=$1;",id)
+func getProductHandler(c *fiber.Ctx) error {
+	produceId, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	product, err := getProduct(produceId)
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
 
-  err := row.Scan(&p.ID,&p.Name,&p.Price)
-  
-  if err != nil{
-    return Product{},err
-  }
-
-
-  return p,nil
+	return c.JSON(product)
 }
 
-func updateProduct (id int ,product *Product) error {
+func getProductsHandler(c *fiber.Ctx) error {
+	product, err := getProducts()
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
 
-  _,err := db.Exec(
-    "UPDATE public.products SET name=$1, price=$2 WHERE id=$3;",
-  product.Name,
-  id,
-  )
+	return c.JSON(product)
+}
 
-  return err
+func createProductHandler(c *fiber.Ctx) error {
+	p := new(Product)
+	if err := c.BodyParser(p); err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
 
+	err := createProduct(p)
+
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	return c.JSON(p)
+}
+
+func updateProductHandler(c *fiber.Ctx) error {
+  id := c.Params("id")
+  p := new(Product)
+  if err := c.BodyParser(p); err != nil {
+    return err
+  }
+
+  // Update product in the database
+  _, err := db.Exec("UPDATE products SET name = $1, price = $2 WHERE id = $3", p.Name, p.Price, id)
+  if err != nil {
+    return err
+  }
+
+  p.ID, _ = strconv.Atoi(id)
+  return c.JSON(p)
+}
+
+func deleteProductHandler(c *fiber.Ctx) error {
+  id := c.Params("id")
+
+  // Delete product from database
+  _, err := db.Exec("DELETE FROM products WHERE id = $1", id)
+  if err != nil {
+    return err
+  }
+
+  return c.SendStatus(fiber.StatusNoContent)
 }
