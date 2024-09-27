a9e3bafcfda59e63a76f00d40d8dfceeaace8d21
Yiren <l6mTbdko9Nza@gmail.com>
2024-09-27 15:02:02 +0700

    fix update cart

--- api/cartapi/cart.go
+++ api/cartapi/cart.go
@@ -1,9 +1,8 @@
 package apicart
 
 import (
-	// model "nack/models"
-	homeapi "nack/api/homeapi"
-	
+	model "nack/models"
+	// homeapi "nack/api/homeapi"
 
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
@@ -11,21 +10,42 @@ import (
 
 type Cart struct {
 	gorm.Model
-	Price     float64 `gorm:"not null" json:"price"`
-	Quantity  int     `gorm:"not null" json:"quantity"`
-	ProductID uint    `gorm:"not null" json:"productID"` // FK referencing Product.ID
-	UserID    uint    `gorm:"not null" json:"userid"`
-	Product   homeapi.Product    // FK referencing User.ID
+	Price              float64 `gorm:"not null" json:"price"`
+	Quantity           int     `gorm:"not null" json:"quantity"`
+	ProductID          uint    `gorm:"not null" json:"productID"` // FK referencing Product.ID
+	UserID             uint    `gorm:"not null" json:"userid"`
+	ProductCode        string  `json:"product_code"`
+	ProductName        string  `json:"product_name"`
+	ProductDescription string  `json:"product_description"`
+	ProductRating      int     `json:"product_rating"`
+	ProductImage       string  `json:"product_image"`
 }
 
 func GetCart(db *gorm.DB, c *fiber.Ctx, userId uint) error {
-	var carts []Cart
+	var carts []model.Cart
+	var cartsResponse []Cart
 
 	if err := db.Preload("Product").Find(&carts, "user_id = ?", userId).Error; err != nil {
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "No products in cart "})
 	}
 
-	return c.JSON(carts)
+	for _, cart := range carts {
+		cartRes := Cart{
+			Price:              cart.Price,
+			Quantity:           cart.Quantity,
+			ProductID:          cart.ProductID,
+			ProductCode:        cart.Product.Code,
+			ProductName:        cart.Product.Name,
+			ProductDescription: cart.Product.Description,
+			ProductRating:      cart.Product.Rating,
+			ProductImage:       cart.Product.Image,
+		}
+
+		// ใช้ append เพื่อเพิ่มค่าเข้าไปใน slice
+		cartsResponse = append(cartsResponse, cartRes)
+	}
+
+	return c.JSON(cartsResponse)
 }
 
 func AddToCart(db *gorm.DB, c *fiber.Ctx, userId uint) error {
@@ -52,7 +72,6 @@ func AddToCart(db *gorm.DB, c *fiber.Ctx, userId uint) error {
 	}
 
 	return c.JSON(fiber.Map{
-		"message": "Products added to cart",
-		"cart":    carts,
+		"message": "Added Products to cart Success",
 	})
 }
